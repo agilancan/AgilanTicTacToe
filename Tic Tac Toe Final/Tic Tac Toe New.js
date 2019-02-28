1e1375e0f42bac9014ea3e8719dffae1be2d088f
agilancan <30876161+agilancan@users.noreply.github.com>
2019-02-28 05:23:26 -0500

    Started work on Linked Lists

--- Tic Tac Toe Final/Tic Tac Toe New.js
+++ Tic Tac Toe Final/Tic Tac Toe New.js
@@ -1,8 +1,62 @@
 /*jshint esversion: 6 */
+// linked list to hold the board values
 
-let x = 5;
-let o = 2;
+function linkedList() {
+    let length = 0;
+    let head = null;
 
-boardShown = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let Node = function(element) {
+        this.element = element;
+        this.next = null;
+    };
 
-//2/27 8:28s
+    this.size = function () {
+        return length;
+    };
+
+    this.head = function () {
+        return head;
+    };
+
+    this.add = function(element) {
+        let node = new Node(element);
+        if(head === null) {
+            head = node;
+        } else {
+            let currentNode = head;
+    
+            while(currentNode.next) {
+                currentNode = currentNode.next;
+            }
+
+            currentNode.next = node;
+        }
+    length++;
+};
+
+    this.remove = function(element) {
+        let currentNode = head;
+        let previousNode;
+        if(currentNode.element === element) {
+            head = currentNode.next;
+        } else {
+            while(currentNode.element !== element) {
+                previousNode = currentNode;
+            }
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
