3fd0fd3f68375119f44fff5c760d18eb7e496fdb
agilancan <30876161+agilancan@users.noreply.github.com>
2019-03-16 19:40:13 -0400

    OOP

--- ticTacToe/ticTacToe.js
+++ ticTacToe/ticTacToe.js
@@ -1,7 +1,16 @@
 /*jshint esversion: 6 */
 
 //The winning moves sequences
-const sequence = ((1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7));
+const winningSequences = [
+    [1, 2, 3], 
+    [4, 5, 6], 
+    [7, 8, 9], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [3, 6, 9], 
+    [1, 5, 9], 
+    [3, 5, 7]
+];
 
 
 //Creating a linked list
@@ -51,14 +60,23 @@ function linkedList() {
         }
     };
 
-    this.neighbourCheck = function() {
-        re
-    };
+//     this.neighbourCheck = function(i, squareNeibourghs) {
+//         if(i > squareNeibourghs.length) {
+//             return null;
+//         } 
+//         return squareNeibourghs + neighbourCheck(squareNeibourghs-1);
+//     };
 
     
+// // For each element in this array
+// //for each element in this array
+// //check if 
 
-
-    
+// for(let i = 0; i > squareNeibourghs.length; i++) {
+//     if(squareNeibourghs[i] === squareNeibourghs[i][j]) {
+//         return 'You win!';
+//     }
+// } return 
 
   
 }
@@ -75,18 +93,7 @@ function linkedList() {
 let board = new linkedList();
 
 
-// All square neighbours
-let squareNeibourghs = {
-    1:[2, 4, 5],
-    2:[1, 3, 5],
-    3:[2, 5, 6],
-    4:[1, 5, 7],
-    5:[4, 1, 2, 3, 6, 7, 8, 9],
-    6:[3, 5, 9],
-    7:[4, 5, 8],
-    8:[7, 5, 9],
-    9:[8, 5, 6]
-}
+
 
 
 
@@ -95,8 +102,10 @@ let squareNeibourghs = {
 // 5. Create a function that will take the three elements from the new variable and match against the 
 // winning sequence patterns (using a linked list neighbour compare function)
 
-
-
+// 5th element
+// 3rd 
+// 1st 
+//currentWiin = [1,2,3]
 
 
 // Setting up the event listener
@@ -396,3 +405,274 @@ userPosition9_div.addEventListener('click', function() {
         }
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// make a linked list called board
+// make each node on that list, a nested linked list with the neighbours as their own nodes
+
+
+
+
+
+
+// Adding nodes to the objectBoard linked list
+// function createNeigbourNodes() {
+//     for(let i = 0; i < 10; i++){
+//         objectBoard.add(i);
+//     }
+// }
+
+
+// All square neighbours
+let squareNeibourghs = {
+    1:[2, 4, 5],
+    2:[1, 3, 5],
+    3:[2, 5, 6],
+    4:[1, 5, 7],
+    5:[4, 1, 2, 3, 6, 7, 8, 9],
+    6:[3, 5, 9],
+    7:[4, 5, 8],
+    8:[7, 5, 9],
+    9:[8, 5, 6]
+}
+
+
+// Defining another board as an object, with a linked list
+let objectBoard = new linkedList();
+
+
+// Creating the nine variables, that will hold the square linked lists
+squareObject1 = new linkedList();
+squareObject2 = new linkedList();
+squareObject3 = new linkedList();
+squareObject4 = new linkedList();
+squareObject5 = new linkedList();
+squareObject6 = new linkedList();
+squareObject7 = new linkedList();
+squareObject8 = new linkedList();
+squareObject9 = new linkedList();
+
+
+// Creating the inner linked links with the neighbours of each square
+objectBoard.add(squareObject1);
+objectBoard.add(squareObject2);
+objectBoard.add(squareObject3);
+objectBoard.add(squareObject4);
+objectBoard.add(squareObject5);
+objectBoard.add(squareObject6);
+objectBoard.add(squareObject7);
+objectBoard.add(squareObject8);
+objectBoard.add(squareObject9);
+
+
+// All squareObject neighbours
+squareObject1.add(squareObject2);
+squareObject1.add(squareObject4);
+squareObject1.add(squareObject5);
+
+squareObject2.add(squareObject3);
+squareObject2.add(squareObject6);
+squareObject2.add(squareObject5);
+squareObject2.add(squareObject4);
+squareObject2.add(squareObject1);
+
+squareObject3.add(squareObject6);
+squareObject3.add(squareObject5);
+squareObject3.add(squareObject2);
+
+squareObject4.add(squareObject1);
+squareObject4.add(squareObject5);
+squareObject4.add(squareObject7);
+
+squareObject5.add(squareObject1);
+squareObject5.add(squareObject2);
+squareObject5.add(squareObject3);
+squareObject5.add(squareObject4);
+squareObject5.add(squareObject6);
+squareObject5.add(squareObject7);
+squareObject5.add(squareObject8);
+squareObject5.add(squareObject9);
+
+squareObject6.add(squareObject3);
+squareObject6.add(squareObject5);
+squareObject6.add(squareObject9);
+
+squareObject7.add(squareObject4);
+squareObject7.add(squareObject5);
+squareObject7.add(squareObject8);
+
+squareObject8.add(squareObject7);
+squareObject8.add(squareObject5);
+squareObject8.add(squareObject9);
+
+squareObject9.add(squareObject8);
+squareObject9.add(squareObject5);
+squareObject9.add(squareObject6);
+
+
+// constructor
+// struct
+
+// let square1 = {
+//     name: name,
+//     rightNeighbour: square2,
+//     downNeighbour
+//     this.leftNeightbour
+//     this.leftDownNeighbour
+//     this.upN
+//     this.upN
+//     this.upN
+//     this.upN
+// }
+
+
+
+
+
+
+
+// this.neighbourCheck = function(i, squareNeibourghs) {
+//     if(i > squareNeibourghs.length) {
+//         return null;
+//     } 
+//     return squareNeibourghs + neighbourCheck(squareNeibourghs-1);
+// };
+
+
+
+
+
+// // Recurive function that will check it's neighbours
+// for(let i = 0; i > squareNeibourghs.length; i++) {
+//     if(squareNeibourghs[i] === squareNeibourghs[i][j]) {
+//         return 'You win!';
+//     }
+// } return 
+
+
+
+
+
+
+
+
+// if first element is equal to the second element
+// then move on to the the neighbouring elements
+
+
+// 1 => 2 (right)
+// 1 => 5 (right, down)
+// 1 => 4 (down)
+
+// 2 => 3 (right)
+// 2 => 6 (right, down)
+// 2 => 5 (down)
+// 2 => 4 (left, down)
+// 2 => 1 (left)
+
+// 3 => 6 (down)
+// 3 => 5 (left, down)
+// 3 => 2 (left)
+
+// 4 => 1 (up)
+// 4 => 5 (right)
+// 4 => 7 (dowm)
+
+// 5 => 1 (left, up)
+// 5 => 2 (up)
+// 5 => 3 (right, up)
+// 5 => 4 (left)
+// 5 => 6 (right)
+// 5 => 7 (left, dowm)
+// 5 => 8 (dowm)
+// 5 => 9 (right, dowm)
+
+// 6 => 3 (right, up)
+// 6 => 5 (left)
+// 6 => 9 (dowm)
+
+// 7 => 4 (up)
+// 7 => 5 (right, up)
+// 7 => 8 (right)
+
+// 8 => 7 (left)
+// 8 => 5 (up)
+// 8 => 9 (right)
+
+// 9 => 8 (left)
+// 9 => 5 (left, up)
+// 9 => 6 (up)
+
+
+
+
+
+// use a square constructor method to check neighbours
+//if neighbour check is true, then continue to the neighbour check in the same direction of the true enighbours
+
+
+function Board(name) {
+    this.constructor = Board,
+    this.name = name
+}
+
+function Square(name, neighbourUp, neighbourRightUp, neighbourRight, neighbourRightDown, neighbourDown, neighbourLeftDown, neighbourLeft, neighbourLeftUp) {
+    this.constructor = Square,
+    this.name = name,
+    this.neighbourUp = neighbourUp,
+    this.neighbourRightUp = neighbourRightUp,
+    this.neighbourRight = neighbourRight,
+    this.neighbourRightDown = neighbourRightDown,
+    this.neighbourDown = neighbourDown,
+    this.neighbourLeftDown = neighbourLeftDown,
+    this.neighbourLeft = neighbourLeft,
+    this.neighbourLeftUp = neighbourLeftUp,
+    this.neighbourCheck = function() {
+        if(this.neighbourUp === true) {
+            if(this.this.neighbourUp === true) {
+                return 'You Win!';
+            }
+        }
+    }
+}
+
+
+// intention is to pass a square function thru this. Makes code cleaner and more accessible
+function neighbourCheck(square) {
+    return square
+}
+
+neighbourCheck(square1);
+
+Square.prototype = Object.create(Board.prototype);
+
+// order matters!!!!
+
+let square1 = new Square('square1', null, null, neighbourRight, neighbourRightDown, neighbourDown, null, null, null);
+let square2 = new Square('square2', neighbourUp, neighbourRightUp, neighbourRight, neighbourRightDown, neighbourDown, neighbourLeftDown, neighbourLeft, neighbourLeftUp);
+let square3 = new Square('square3', neighbourUp, neighbourRightUp, neighbourRight, neighbourRightDown, neighbourDown, neighbourLeftDown, neighbourLeft, neighbourLeftUp);
+let square4 = new Square('square4', neighbourUp, neighbourRightUp, neighbourRight, neighbourRightDown, neighbourDown, neighbourLeftDown, neighbourLeft, neighbourLeftUp);
+let square5 = new Square('square5', neighbourUp, neighbourRightUp, neighbourRight, neighbourRightDown, neighbourDown, neighbourLeftDown, neighbourLeft, neighbourLeftUp);
+let square6 = new Square('square6', neighbourUp, neighbourRightUp, neighbourRight, neighbourRightDown, neighbourDown, neighbourLeftDown, neighbourLeft, neighbourLeftUp);
+let square7 = new Square('square7', neighbourUp, neighbourRightUp, neighbourRight, neighbourRightDown, neighbourDown, neighbourLeftDown, neighbourLeft, neighbourLeftUp);
+let square8 = new Square('square8', neighbourUp, neighbourRightUp, neighbourRight, neighbourRightDown, neighbourDown, neighbourLeftDown, neighbourLeft, neighbourLeftUp);
+let square9 = new Square('square9', neighbourUp, neighbourRightUp, neighbourRight, neighbourRightDown, neighbourDown, neighbourLeftDown, neighbourLeft, neighbourLeftUp);
+
+square1.prototype.neighbourUp = asdfasdf;
+
+// 2 => 3 (right)
+// 2 => 6 (right, down)
+// 2 => 5 (down)
+// 2 => 4 (left, down)
+// 2 => 1 (left)
